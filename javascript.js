fe8c440304373e8390005d75fcf82cba0291ba9d
nareshix <nareshix66@gmail.com>
2024-09-02 17:32:43 +0800

    displays necessary text when typing password

--- javascript.js
+++ javascript.js
@@ -5,5 +5,24 @@ const displayStrength = document.querySelector('.display-strength')
 let password = '';
 input.addEventListener('keyup', () => {
     password = input.value;
-    let result = zxcvbn(password)
+    let result = zxcvbn(password, user_inputs=[])
+    
+    // chose offline_fast_hashing_1e10_per_second to expect worst case scenario
+    crackingTime.textContent = result.crack_times_display.offline_slow_hashing_1e4_per_second;    
+    switch (result.score) {
+        case 0:
+            displayStrength.textContent = 'Very Weak'
+            break;
+        case 1:
+            displayStrength.textContent = 'Weak'
+            break;
+        case 3:
+            displayStrength.textContent = 'Strong'
+            break;
+        case 4:
+            displayStrength.textContent = 'Very Strong'
+            break;
+        }
+    
+     
 })
